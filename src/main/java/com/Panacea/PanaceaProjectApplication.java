3f925368af68448ba13bb6d0758a468fc0592141
x532410829 <532410829@qq.com>
2020-09-09 10:51:58 +0800

    首次提交

---
+++ src/main/java/com/Panacea/PanaceaProjectApplication.java
@@ -0,0 +1,22 @@
+package com.Panacea;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 万能药项目------Panacea
+ * 目的：将所有用过技术，做成单个零件式的demo，以后可以复制修改部分地方即可使用，帮助快速开发建立项目
+ * 		 尽量做到所有代码都带上详细的注释、功能介绍、引用的包、注意事项等
+ * 
+ * @author 夜未
+ * @since 2020年9月9日
+ */
+
+@SpringBootApplication
+public class PanaceaProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PanaceaProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Panacea/PanaceaProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Panacea;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PanaceaProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
