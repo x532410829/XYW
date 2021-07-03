8acde6b6595c7ad6a8dc85a407f6cd8616f83f8c
x532410829 <532410829@qq.com>
2021-07-03 15:18:32 +0800

    增加动态修改日志级别的功能demo

--- src/main/java/com/Panacea/demo/LogDemo.java
+++ src/main/java/com/Panacea/demo/LogDemo.java
@@ -2,6 +2,8 @@ package com.Panacea.demo;
 
 import java.io.File;
 
+import javax.annotation.PostConstruct;
+
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -9,6 +11,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.Panacea.unity.util.Result;
+
+import ch.qos.logback.classic.Level;
+import ch.qos.logback.classic.LoggerContext;
 
 /**
  * 日志
@@ -47,6 +52,68 @@ public class LogDemo {
 	 public int log(int num) {		 
 		 return 10/num;
 	 }
+	 
+	 
+	 @PostConstruct
+	 public void generLog() {
+		 Thread thread=new Thread(new Runnable() {
+			
+			@Override
+			public void run() {
+				 while(true) {
+				        logger.info("-----Info 日志...");
+				        logger.error("-----Error 日志...");
+				        try {
+							Thread.sleep(5000);
+						} catch (InterruptedException e) {
+							e.printStackTrace();
+						}
+				 }
+			}
+		});
+		 
+		 thread.start();
+		 
+	 }
+	 
+	 
+	 /**
+	  * 运行时动态修改日志级别
+	  * @param level ERROR  INFO
+	  * @return
+	  */
+	 @RequestMapping("level")
+	 public String changeLevel(String level) {	
+		 
+		 try {
+			 
+			 System.out.println("设置参数："+level);
+			 
+			 LoggerContext loggerContext = (LoggerContext) LoggerFactory.getILoggerFactory();
+//			 loggerContext.getLogger("org.mybatis").setLevel(Level.valueOf(level));
+//			 loggerContext.getLogger("org.springframework").setLevel(Level.valueOf(level));
+			 
+			 Logger  logger=loggerContext.getLogger("root");
+			 
+			 
+			 ((ch.qos.logback.classic.Logger)logger).setLevel(ch.qos.logback.classic.Level.toLevel(level));
+			 
+			 
+			 
+			 String info=((ch.qos.logback.classic.Logger)logger).getLevel().levelStr;
+			 
+			 return "success:"+info;
+
+			 } catch (Exception e) {
+			 logger.error("动态修改日志级别出错", e);
+
+			 return "fail";
+
+			 }
+
+	 }
+		 
+	 
 	 
 	 
 	 
